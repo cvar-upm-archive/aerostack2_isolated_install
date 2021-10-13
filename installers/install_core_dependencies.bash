6ec5600ebc17039f6bd34a65eff240ca9883d5c0
Miguel <miferco97@gmail.com>
2021-10-13 10:53:06 +0200

    update packages

--- installers/install_core_dependencies.bash
+++ installers/install_core_dependencies.bash
@@ -8,6 +8,7 @@ sudo apt install ros-${ROS_DISTRO}-ament-cmake-clang-format -y &&\
 sudo apt install ros-${ROS_DISTRO}-eigen3-cmake-module -y &&\
 sudo pip3 install -U empy pyros-genmsg setuptools &&\
 sudo apt install libgoogle-glog-dev &&\
+sudo apt install libnlopt-cxx-dev &&\
 sudo usermod -a -G dialout $USER &&\
 sudo apt-get remove modemmanager -y&&\
 sudo apt install gstreamer1.0-plugins-bad gstreamer1.0-libav gstreamer1.0-gl -y &&\
