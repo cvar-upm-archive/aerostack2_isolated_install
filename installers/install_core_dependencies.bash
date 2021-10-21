b7805dabc670199ace233549266438411b60f41b
Miguel <miferco97@gmail.com>
2021-10-21 16:43:11 +0200

    added gazebo-plugins

--- installers/install_core_dependencies.bash
+++ installers/install_core_dependencies.bash
@@ -6,6 +6,7 @@ sudo apt install tmux python3-pip python3-testresources -y && \
 sudo apt install python3-colcon-common-extensions -y && \
 sudo apt install ros-${ROS_DISTRO}-ament-cmake-clang-format -y &&\
 sudo apt install ros-${ROS_DISTRO}-eigen3-cmake-module -y &&\
+sudo apt install ros-${ROS_DISTRO}-gazebo-ros-pkgs -y &&\
 sudo apt-get install -y libeigen3-dev -y &&\
 sudo pip3 install -U empy pyros-genmsg setuptools &&\
 sudo apt install libgoogle-glog* -y &&\
@@ -16,3 +17,5 @@ sudo usermod -a -G dialout $USER &&\
 sudo apt-get remove modemmanager -y&&\
 sudo apt install gstreamer1.0-plugins-bad gstreamer1.0-libav gstreamer1.0-gl -y &&\
 sudo udevadm control --reload-rules && udevadm trigger
+
+source $HOME/.bashrc
