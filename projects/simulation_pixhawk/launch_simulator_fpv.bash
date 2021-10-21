efaa44657c1cdc7abd0c09d8c362c6d441af9a25
Miguel <miferco97@gmail.com>
2021-10-21 16:28:34 +0200

    fpv world

---
+++ projects/simulation_pixhawk/launch_simulator_fpv.bash
@@ -0,0 +1,34 @@
+#!/bin/bash
+[ -d "/usr/share/gazebo-7" ] && export GAZEBO_RESOURCE_PATH=/usr/share/gazebo-7
+[ -d "/usr/share/gazebo-9" ] && export GAZEBO_RESOURCE_PATH=/usr/share/gazebo-9
+[ -d "/usr/share/gazebo-11" ] && export GAZEBO_RESOURCE_PATH=/usr/share/gazebo-11
+
+AEROSTACK_PROJECT=$(pwd)
+
+#SIMULATE WITHOUT CAMERA
+	#(cd $(rospack find px4) ; make px4_sitl gazebo)
+
+#SIMULATE WITH CAMERA
+
+
+WORLD_FILE="${AEROSTACK_PROJECT}/configs/gazebo/worlds/frames2.world"
+UAV_NAME="iris"
+MODEL_FOLDER="${AEROSTACK_PROJECT}/configs/gazebo/models"
+
+cp "$MODEL_FOLDER/$UAV_NAME/iris.sdf" "$MODEL_FOLDER/$UAV_NAME/iris.sdf.jinja"
+cp "$MODEL_FOLDER/$UAV_NAME/iris.sdf" "$MODEL_FOLDER/$UAV_NAME/iris.sdf.last_generated"
+
+PX4_FOLDER="$AEROSTACK2_WORKSPACE/src/thirdparty/PX4-Autopilot"
+
+(cd $PX4_FOLDER; DONT_RUN=1 make px4_sitl_rtps gazebo)
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$PX4_FOLDER:$PX4_FOLDER/Tools/sitl_gazebo
+export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$MODEL_FOLDER
+export PX4_SITL_WORLD=$WORLD_FILE
+export GAZEBO_PLUGIN_PATH=$PX4_FOLDER/build/px4_sitl_rtps/build_gazebo
+# export GAZEBO_MODEL_PATH=${AEROSTACK_PROJECT}/configs/gazebo/models
+# source $PX4_FOLDER/Tools/setup_gazebo.bash $PX4_FOLDER $PX4_FOLDER/build/px4_sitl_rtps
+source $PX4_FOLDER/build/px4_sitl_rtps/build_gazebo/setup.sh
+
+# bash launch_simulator.bash
+$PX4_FOLDER/Tools/sitl_run.sh "$PX4_FOLDER/build/px4_sitl_rtps/bin/px4" none gazebo none none $PX4_FOLDER $PX4_FOLDER/build/px4_sitl_rtps
+pkill gzserver;pkill gzclient
