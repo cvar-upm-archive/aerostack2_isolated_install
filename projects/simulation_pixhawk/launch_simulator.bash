7dc38116aaf0a37fc02d78a76e73686d686dd769
Pedro Arias <pedroariasperez96@gmail.com>
2022-02-16 13:44:58 +0100

    undoing bad fix

--- projects/simulation_pixhawk/launch_simulator.bash
+++ projects/simulation_pixhawk/launch_simulator.bash
@@ -3,7 +3,7 @@
 #https://github.com/aerostack2-developers/PX4-Autopilot.git
 
 
-PX4_FOLDER="$AEROSTACK2_WORKSPACE/src/aerostack2/thirdparty/PX4-Autopilot"
+PX4_FOLDER="$AEROSTACK2_WORKSPACE/src/thirdparty/PX4-Autopilot"
 
 cd $PX4_FOLDER &&
     make px4_sitl_rtps gazebo
