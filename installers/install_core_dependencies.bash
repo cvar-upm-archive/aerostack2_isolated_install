91d0200b80b7881300742f78cdfa1cb644c4f5ae
Miguel <miferco97@gmail.com>
2022-09-26 13:10:27 +0200

    removed specific dependencies

--- installers/install_core_dependencies.bash
+++ installers/install_core_dependencies.bash
@@ -21,19 +21,20 @@ sudo apt install python3-colcon-common-extensions -y
 
 sudo apt install ros-${ROS_DISTRO}-ament-cmake-clang-format -y &&\
 sudo apt install ros-${ROS_DISTRO}-eigen3-cmake-module -y &&\
-sudo apt install ros-${ROS_DISTRO}-gazebo-ros-pkgs -y &&\
-sudo apt install ros-${ROS_DISTRO}-geographic-msgs -y
+
+# sudo apt install ros-${ROS_DISTRO}-gazebo-ros-pkgs -y &&\
+# sudo apt install ros-${ROS_DISTRO}-geographic-msgs -y
 
 # geographic lib
-sudo apt install libgeographic-dev geographiclib-tools -y &&\
-sudo ln -s /usr/share/cmake/geographiclib/FindGeographicLib.cmake /usr/share/cmake-*/Modules/ &&\
-sudo pip3 install -U empy pyros-genmsg setuptools jinja2
+# sudo apt install libgeographic-dev geographiclib-tools -y &&\
+# sudo ln -s /usr/share/cmake/geographiclib/FindGeographicLib.cmake /usr/share/cmake-*/Modules/ &&\
+# sudo pip3 install -U empy pyros-genmsg setuptools jinja2
 
 # libgoogle utils
 
-sudo apt install libgoogle-glog* -y &&\
-sudo apt install libgflags* -y &&\
-sudo apt install libnlopt-cxx-dev -y
+# sudo apt install libgoogle-glog* -y &&\
+# sudo apt install libgflags* -y &&\
+# sudo apt install libnlopt-cxx-dev -y
 
 # USB Drivers
 # sudo usermod -a -G dialout $USER &&\
